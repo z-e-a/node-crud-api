04b39b95c0c3b39663c1930af1f781b82751c12e
z-e-a <e.a.zubkov@yandex.ru>
2024-10-21 19:37:12 +0300

    feat: add database service

---
+++ src/store/initialDbContent.ts
@@ -0,0 +1,10 @@
+import { IUser } from 'src/types/entity';
+
+export default [
+  {
+    id: '3bea8196-3c6e-48fc-b7dc-e070b10c5c76',
+    username: 'z-e-a',
+    age: 44,
+    hobbies: ['programming', 'guitar', 'snowboard'],
+  },
+] as IUser[];
